362efcbb1f28d0348267a5d5c20e6f5b6dbe89b0
Ahmed <131725815+AhmedTools@users.noreply.github.com>
2023-07-05 19:09:07 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ def working(message):
             button_markup = telebot.types.InlineKeyboardButton(text=f'- {text}', callback_data='text')
             markup_text.add(button_markup)
             bot.send_photo(message.chat.id, photo=image, reply_markup=markup_text)
-            print(f"{message.chat.id} : {text}")
+            print(f"Name : {message.from_user.first_name} | Id : {message.chat.id} | Text : {text}")
         elif any(char in symbols or char in numbers for char in text):
             bot.send_chat_action(message.chat.id, 'find_location')
             image = requests.get(f"http://apis.xditya.me/write", {'text': text}).content
@@ -76,7 +76,7 @@ def working(message):
             button_markup = telebot.types.InlineKeyboardButton(text=f'- {text}', callback_data='text')
             markup_text.add(button_markup)
             bot.send_photo(message.chat.id, photo=image, reply_markup=markup_text)
-            print(f"{message.chat.id} : {text}")
+            print(f"Name : {message.from_user.first_name} | Id : {message.chat.id} | Text : {text}")
         else:
             bot.send_message(message.chat.id, "- Sorry The Bot Not Support This Language\n\n- عذراً هذا البوت لا يدعم هذه اللغة", reply_markup=markup)
     last_message = text
